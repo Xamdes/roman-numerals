f20905185038b668b06f8a52a91c77970fe978c6
Colin Bates and Steven Colburn <colin@>
2018-06-07 15:51:31 -0700

    removed god awful complicated forsaken code

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,30 @@
-var romanStrings = ["I","V","X","L","C","D","M"];
-var romanIntegers = [1,5,10,50,100,500,1000];
-//Team Code
+var romanStrings = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+var romanIntegers = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+
 $(function()
 {
-  //debugger
   $("#form-one").submit(function(event)
   {
-    event .preventDefault();
-    console.log("------------------------");
-
-    var romanCounter = [0,0,0,0,0,0,0];
-    var romanIntNumElements = (romanIntegers.length-1);
+    event.preventDefault();
+    var romanIntNumElements = (romanIntegers.length);
     var output = "";
     var input = parseInt($("#input").val().trim());
-    var inputArray = input.toString().split("");
 
-    for(var indexTop = 0; indexTop <= inputArray.length;indexTop++)
+    if(input<4000)
     {
-
-      console.log("Index: "+indexTop+" | array: "+inputArray+" | input: "+input);
-      var tempSwitch = inputArray[indexTop];
-
-
-      if(tempSwitch === NaN || tempSwitch === undefined)
+      for(var index = romanIntNumElements; index >=0; index--)
       {
-        break;
-      }
-
-      console.log(tempSwitch);
-
-      if(tempSwitch==="4")
-      {
-        // outputTest += ""; if index 1(replace LLLL with  LC ){
-        //   elseif index 2(replace vvvv with vx )
-        // }
-        output += "";
-        console.log("4"+inputArray.shift());
-        input = parseInt(inputArray.join(""));
-        indexTop = -1;
-      }
-      else if(tempSwitch==="9")
-      {
-        output += "";
-        console.log("9"+inputArray.shift());
-        input = parseInt(inputArray.join(""));
-        indexTop = -1;
-      }
-      else
-      {
-        for(var index = romanIntNumElements; index >=0 ; index--)
+        var value = romanIntegers[index];
+        var temp = input-value;
+        if(temp>=0)
         {
-          var value = romanIntegers[index];
-          console.log(input);
-          var temp = input-value;
-          if(temp>=0)
-          {
-            console.log("Val: "+value+" Temp: "+temp);
-            input = temp;
-            romanCounter[index]++;
-            index = romanIntNumElements;
-            output += (convertToRoman(value).toString());
-          }
+          input = temp;
+          index = romanIntNumElements;
+          output += (convertToRoman(value).toString());
         }
       }
     }
 
-
-    //output = isRomanNumeral(input,romanStrings,romanIntegers);
-    //console.log(romanCounter);
     $("#output").text(output);
     //$("#input").val("");
   });
@@ -84,11 +41,4 @@ function convertToRoman(input)
   {
     return -1;
   }
-}
-
-
-
-function findLocation(value,tempArray)
-{
-  return tempArray.indexOf(value);
 }
--- js/test-code.js
+++ js/test-code.js
@@ -1,18 +1,18 @@
-if input=4 for(i =0;     ;i++)
-if index 2 replace llll with lc
-if index 1 replace vvvv  with vx
-if index 0 replace iiii with iv
-
- var nine = input.indexOf(9)
-if =9 check index length
-if index 2 put cm
-if endex 1 put xc
-if index 0 put ix
-if find 4 in the number get the location in the number then replace
-
-
-find place in number thousands would be 900 would be "CM"
-90 would be "XC"
+// if input=4 for(i =0;     ;i++)
+// if index 2 replace llll with lc
+// if index 1 replace vvvv  with vx
+// if index 0 replace iiii with iv
+//
+//  var nine = input.indexOf(9)
+// if =9 check index length
+// if index 2 put cm
+// if endex 1 put xc
+// if index 0 put ix
+// if find 4 in the number get the location in the number then replace
+//
+//
+// find place in number thousands would be 900 would be "CM"
+// 90 would be "XC"
 
 
 
