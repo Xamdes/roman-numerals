018ad9183e8d5e98b86baeaf2c566b3a7d6db712
Colin Bates and Steven Colburn <colin@>
2018-06-07 10:00:09 -0700

    turned notes into code

--- js/scipts.js
+++ js/scipts.js
@@ -1,25 +1,15 @@
-$(document).ready(function(){
-$("form-id").submit(function(event){
-
-var I = 1
-var V = 5
-var X = 10
-var L = 50
-var C = 100
-var D = 500
-var M = 1000
- input text
-
-var romanN = ("I" "V" "X" "L" "C" "D" "M")
-roman2=indexof(romanN)
-
-for (i = 0; romanN > )
-  i\romanN.
-  if inputN repeats more than 3 times replace with index ++ prepend index --
-  99 = XCIX
+$(function()
+{
+  $("form-id").submit(function(event)
+  {
+    event.preventDefault();
+    var romanStrings = ["I","V","X","L","C","D","M"];
+    var romanIntegers = [1,5,10,50,100,500,1000];
 
 
+    for (var index = 0;false;index++)
+    {
 
-
-  }
-  }
+    }
+  });
+});
