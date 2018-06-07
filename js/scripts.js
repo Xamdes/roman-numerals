7ee4d2547f34d009414490341901b5a64df6b5a5
Colin Bates and Steven Colburn <colin@>
2018-06-07 11:09:37 -0700

    add for loop to go thru user input and output roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,41 @@ $(function()
   $("#form-one").submit(function(event)
   {
     event.preventDefault();
-    var output = "Error";
+    var romanStrings = ["I","V","X","L","C","D","M"];
+    var romanIntegers = [1,5,10,50,100,500,1000];
+    var romanIntNumElements = (romanIntegers.length-1);
+    var output = "";
     var input = parseInt($("#input").val().trim());
-    console.log(input);
+    console.log("Input: "+input);
 
-    output = isRomanNumeral(input);
+    for(var index = romanIntNumElements; index >=0; index--)
+    {
+      var value = romanIntegers[index]
+      var temp = input-value;
+      console.log("Val: "+value+" Temp: "+temp);
+      if(temp>=0)
+      {
+        input = temp;
+        index = romanIntNumElements;
+        output += (isRomanNumeral(value,romanStrings,romanIntegers).toString());
+      }
+    }
+
+
+
+    //output = isRomanNumeral(input,romanStrings,romanIntegers);
 
     $("#output").text(output);
-    $("#input").val("");
+    //$("#input").val("");
   });
 });
 
-function isRomanNumeral(input)
+function isRomanNumeral(input,ptrRomanStrings,ptrRomanIntegers)
 {
-  var romanStrings = ["I","V","X","L","C","D","M"];
-  var romanIntegers = [1,5,10,50,100,500,1000];
-  var findIndex = romanIntegers.indexOf(input);
+  var findIndex = ptrRomanIntegers.indexOf(input);
   if(findIndex >= 0)
   {
-    return romanStrings[findIndex];
+    return ptrRomanStrings[findIndex];
   }
   else
   {
